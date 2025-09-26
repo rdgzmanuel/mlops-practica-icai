9c149268450326f57390649cb2cb3a72a898126e
rdgzmanuel <manuel.rodriguezvillegas09@gmail.com>
2025-09-26 08:20:58 +0200

    Updated train and dataset.

--- train.py
+++ train.py
@@ -24,24 +24,27 @@
 
 # print("Modelo entrenado y guardado como 'model.pkl'")
 
-
-
+import os
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 import joblib
 import mlflow
 import mlflow.sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-import dagshub
-dagshub.init(repo_owner='rdgzmanuel', repo_name='mlops-practica-icai', mlflow=True)
+# Cargar el conjunto de datos desde el archivo CSV
+try:
+    iris = pd.read_csv('data/iris_dataset.csv')
+except FileNotFoundError:
+    print("Error: El archivo 'data/iris_dataset.csv' no fue encontrado.")
 
-# Cargar el conjunto de datos
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+# Dividir el DataFrame en características (X) y etiquetas (y)
+X = iris.drop('target', axis=1)
+y = iris['target']
 # Iniciar un experimento de MLflow
 with mlflow.start_run():
     # Dividir los datos en conjuntos de entrenamiento y prueba
