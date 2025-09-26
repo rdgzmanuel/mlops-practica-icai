552c94d4c184e36fbadeeb71ee1320702a9f8223
rdgzmanuel <manuel.rodriguezvillegas09@gmail.com>
2025-09-26 08:37:30 +0200

    Confusion matrix included.

--- train.py
+++ train.py
@@ -36,6 +36,9 @@ import mlflow.sklearn
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+tracking_uri = os.environ.get("MLFLOW_TRACKING_URI")
+mlflow.set_tracking_uri(tracking_uri)
+
 # Cargar el conjunto de datos desde el archivo CSV
 try:
     iris = pd.read_csv('data/iris_dataset.csv')
@@ -45,6 +48,7 @@ except FileNotFoundError:
 # Dividir el DataFrame en características (X) y etiquetas (y)
 X = iris.drop('target', axis=1)
 y = iris['target']
+n_estimators: int = 100
 # Iniciar un experimento de MLflow
 with mlflow.start_run():
     # Dividir los datos en conjuntos de entrenamiento y prueba
@@ -52,7 +56,7 @@ with mlflow.start_run():
     X, y, test_size=0.3, random_state=42
     )
     # Inicializar y entrenar el modelo
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
     model.fit(X_train, y_train)
     # Realizar predicciones y calcular la precisión
     y_pred = model.predict(X_test)
@@ -61,9 +65,22 @@ with mlflow.start_run():
     # Guardar el modelo entrenado en un archivo .pkl
     joblib.dump(model, 'model.pkl')
     # Registrar el modelo con MLflow
-    mlflow.sklearn.log_model(model, "random-forest-model")
+    # mlflow.sklearn.log_model(model, "random-forest-model")
     # Registrar parámetros y métricas
-    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("n_estimators", n_estimators)
     mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_artifact("model.pkl")
     print(f"Modelo entrenado y precisión: {accuracy:.4f}")
     print("Experimento registrado con MLflow.")
+
+    # --- Sección de Reporte para CML ---
+    # 1. Generar la matriz de confusión
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title('Matriz de Confusión')
+    plt.xlabel('Predicciones')
+    plt.ylabel('Valores Reales')
+    plt.savefig('confusion_matrix.png')
+    print("Matriz de confusión guardada como 'confusion_matrix.png'")
+    # --- Fin de la sección de Reporte ---
